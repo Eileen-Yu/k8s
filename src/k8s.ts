75cf95d42c670b91259a6000073e7b2c0065dd31
Tony <kavinjsir@gmail.com>
2022-03-11 01:53:34 -0500

    style: Cleanup job declaration

--- src/k8s.ts
+++ src/k8s.ts
@@ -10,32 +10,34 @@ const k8sJobApi = kc.makeApiClient(k8s.BatchV1Api);
 
 export async function createK8sJob(projectLink: string, namespace: string): Promise<k8s.V1Job | undefined> {
   const jobName = 'myjob';
+
   const job = new k8s.V1Job();
-  const metadata = new k8s.V1ObjectMeta();
+
   job.apiVersion = 'batch/v1';
   job.kind = 'Job';
-  metadata.name = jobName;
-  metadata.annotations = {
-    'packageHunter': 'local',
-  };
-  job.metadata = metadata;
 
-  const spec = new k8s.V1JobSpec();
-  spec.template = {
-    spec: {
-      containers: [
-        {
-          name: "test",
-          image: "busybox",
-          args: ["/bin/sh", "-c", `date; ${projectLink}`]
-        }
-      ],
-      restartPolicy: "Never"
-    }
+  job.metadata = {
+    name: jobName,
+    annotations: {
+      'packageHunter': 'local',
+    },
   };
-  spec.backoffLimit = 0;
 
-  job.spec = spec;
+  job.spec = {
+    template: {
+      spec: {
+        containers: [
+          {
+            name: "test",
+            image: "busybox",
+            args: ["/bin/sh", "-c", `date; ${projectLink}`]
+          }
+        ],
+        restartPolicy: "Never"
+      }
+    },
+    backoffLimit: 0
+  };
 
   console.log(JSON.stringify(job, null, 2));
 
