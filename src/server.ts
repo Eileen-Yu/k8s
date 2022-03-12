12b3b410c4fce409e7b8a403c37fcfe2369b9c20
Eileen <48944635+Eileen-Yu@users.noreply.github.com>
2022-03-12 14:30:01 -0500

    refact: API & test /task query (#1)

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,27 @@ import { router } from './router';
 import { TaskStore } from './types';
 
 export const taskStore: TaskStore = {
-  registeredTasks: { "fakejob1": { status: "not started" } },
+  registeredTasks: { 
+    "abcd123": { 
+      id: "abcd123",
+      projectName: "fake1",
+      projectLink: "https://github.com/Eileen-Yu/k8s",
+      createTime: "3/12/22",
+      finishTime: "3/12/22", 
+      status: "finished",
+      result: [{"alert1": "abc"}, {"alert2": "abcd"}, {"alert3": "abcde"}]
+    },
+    "efg321":{
+      id: "efg321",
+      projectName: "realFake2",
+      projectLink: "www.hellogugu.com",
+      createTime: "3/13/23",
+      finishTime: "",
+      status: "not started" ,
+      result: [{"alert1": 123}, {"alert2": 456}]
+    },
+  },
+
   k8sJobs: { "fakejob1": { podID: "asdfasdfasdfasdfasdf" } }
 };
 
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,22 @@
+interface ProjectInfo {
+  projectName: string;
+  projectLink: string;
+}
+
 export type TaskID = string;
 
+export interface Alert {
+  [key: string]: any
+}
+
 export interface TaskInfo {
+  id: string;
+  projectName: string;
+  projectLink: string;
+  createTime: string;
+  finishTime: string;
   status: string;
+  result: Alert[];
 }
 
 export interface K8sJobInfo {
@@ -12,3 +27,5 @@ export interface TaskStore {
   registeredTasks: Record<TaskID, TaskInfo>;
   k8sJobs: Record<TaskID, K8sJobInfo>;
 }
+
+export {ProjectInfo}
