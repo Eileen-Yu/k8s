228726ad04ff64020b372d9673f4f84c8a2a1acd
Eileen <eileenylj@gmail.com>
2023-05-05 16:47:59 -0400

    learn tf

--- terraform-GCP/main.tf
+++ terraform-GCP/main.tf
@@ -15,10 +15,21 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
+resource "google_compute_network" "my_access" {
+  name = "my-access"
+}
+
+resource "google_compute_firewall" "my_ssh" {
+  name = "allow-my-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.my_access.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
 }
 
 resource "google_compute_instance" "vm_instance" {
@@ -28,14 +39,34 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "cos-cloud/cos-stable"
+      image = "debian-cloud/debian-11"
     }
   }
 
+  metadata_startup_script = <<-EOT
+  sudo apt-get update;
+  sudo apt-get install -yq build-essential python3-pip rsync;
+  pip install flask
+  EOT
+
+  metadata = {
+    ssh-keys = "teemo:${file("/Users/eileen/.ssh/id_ed25519.pub")}"
+  }
+
+  # allow_stopping_for_update = true
+
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network = google_compute_network.my_access.name
     access_config {
     }
   }
 }
 
+output "SSH-IP" {
+  value = join("", ["http://", google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip, ":22"])
+}
+// A variable for extracting the external IP address of the VM
+
+# output "Web-server-URL" {
+#  value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
+# }
